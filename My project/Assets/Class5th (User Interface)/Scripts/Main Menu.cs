a18d502142c1082d1e371782610bd67750ae65c5
ㅇㅇ <gk01084410865@gmail.com>
2025-12-16 19:47:52 +0900

    (2025.12.16) 인터페이스

---
+++ My project/Assets/Class5th (User Interface)/Scripts/Main Menu.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Execute()
+    {
+        Debug.Log("START");
+    }
+
+    public void Load()
+    {
+        Debug.Log("LOAD");
+    }
+
+    public void Exit()
+    {
+        Debug.Log("EXIT");
+    }
+}
