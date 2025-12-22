6209bbf7a97422e37f45c81e4f306ff4b9dffb67
ㅇㅇ <gk01084410865@gmail.com>
2025-12-22 19:45:55 +0900

    (2025.121.22) 입력 시스템

---
+++ My project/Assets/Class9th (Input System)/Scripts/Cheracter.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Cheracter : MonoBehaviour
+{
+    [SerializeField] Vector3 direction;
+    [SerializeField] float speed;
+    void Update()
+    {
+        direction.x = Input.GetAxisRaw("Horizontal");
+        direction.z = Input.GetAxisRaw("Vertical");
+
+        transform.position = transform.position + direction * speed * Time.deltaTime;
+    }
+}
