14058ba956d9b836e2ba5dbe695f16c60ca62736
ㅇㅇ <gk01084410865@gmail.com>
2025-12-30 19:46:30 +0900

    (2025.12.30) 가비지 컬렉터

---
+++ My project/Assets/Class12th (Coroutine)/Scripts/Create Manager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class CreateManager : MonoBehaviour
+{
+    [SerializeField] GameObject prefab;
+    [SerializeField] float time;
+
+    void Start()
+    {
+       GameObject clone = Instantiate(prefab);
+
+       clone.transform.rotation = Quaternion.Euler(0f, 180.0f, 0f);
+    }
+
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time >= 5.0f)
+        {
+            GameObject clone = Instantiate(prefab);
+
+            clone.transform.rotation = Quaternion.Euler(0f, 180.0f, 0f);
+
+            time = 0.0f;
+        }
+    }
+}
