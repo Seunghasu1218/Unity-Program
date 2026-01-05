7c38a7db43efeb425a505297203bf49598e8f735
ㅇㅇ <gk01084410865@gmail.com>
2026-01-05 19:46:03 +0900

    (2026.1.5) 렌더링 파이프 라인

--- My project/Assets/Class13th(Sound)/Scripts/Unit.cs
+++ My project/Assets/Class13th(Sound)/Scripts/Unit.cs
@@ -1,12 +1,49 @@
 using UnityEngine;
 
+public enum AnimationState
+{
+    Idle,
+    Walk,
+    Attack,
+    Die
+}
+
 public class Unit : MonoBehaviour
 {
     [SerializeField] AudioSource audioSource;
+    [SerializeField] Animator animator;
+    [SerializeField] int index = 0;
+    [SerializeField] AnimationState state;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void Transtion(int count)
+    {
+        if (count == 0)
+        {
+            state = (AnimationState)((int)state % 3);
+
+            return;
+        }
+
+        index += count;
+
+        if (index % 3 == 0)
+        {
+            state++;
+        }
+    }
 
     public void Enit()
     {
-        audioSource.clip = Resources.Load<AudioClip>("Attack");
+        Debug.Log(state.ToString());
+
+        Debug.Log(animator.GetCurrentAnimatorStateInfo(0).IsName(state.ToString()));
+
+        audioSource.clip = Resources.Load<AudioClip>(state.ToString());
 
         audioSource.Play();
     }
